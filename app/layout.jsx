e395deb4cbd57bdf8998cf9eb5d68d1c2b8e94c9
Segun Ajibola <ajbl.segun@gmail.com>
2023-03-04 01:25:49 +0100

    firstCommit

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body className={`${montserrat.className} m-32`}>{children}</body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,23 @@
+import Movie from "./Movie"
+
+export default async function Home() {
+  const data = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`
+  )
+  const res = await data.json()
+  return (
+    <main>
+      <div className="grid grid-cols-fluid gap-16">
+        {res.results.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            poster_path={movie.poster_path}
+            release_date={movie.release_date}
+          />
+        ))}
+      </div>
+    </main>
+  )
+}
